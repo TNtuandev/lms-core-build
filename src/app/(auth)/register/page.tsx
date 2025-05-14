6108942945605715908e16d3c170694a6539ea91
devpro20 <devfreelancer885@gmail.com>
2025-05-15 00:40:47 +0700

    feat: add new ui login and register

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,7 +1,73 @@
 import React from "react";
+import Image from "next/image";
+import {bannerSignIn, logoGoogle, logoMini} from "@/contants/images";
 
 function RegisterPage() {
-  return <div>RegisterPage</div>;
+  return (
+    <div className="flex w-full flex-col md:flex-row">
+      <Image
+        src={logoMini}
+        alt="logmini"
+        className="md:hidden block h-full m-4 mb-10"
+      />
+      <Image
+        src={bannerSignIn}
+        alt="banner"
+        className="h-screen object-cover hidden md:block"
+      />
+      <div className="flex flex-col justify-center items-center w-full px-6 md:px-8">
+        <div className="text-[#212B36] font-semibold text-2xl md:text-3xl mb-3">
+          Bắt đầu hoàn toàn miễn phí
+        </div>
+        <div className="flex gap-2 text-sm text-[#212B36]">
+          <div>Bạn đã có tài khoản?</div>
+          <div className="text-[#2F57EF]">Đăng nhập</div>
+        </div>
+        <div className="w-full flex flex-col justify-center items-center gap-4 mt-[40px]">
+          <div className="flex gap-4 w-full items-center">
+            <input
+              type="text"
+              placeholder="Tên"
+              className="w-full border border-gray-200 rounded-md px-4 py-2 h-12"
+            />
+            <input
+              type="text"
+              placeholder="Họ"
+              className="w-full border border-gray-200 rounded-md px-4 py-2 h-12"
+            />
+          </div>
+          <input
+            type="text"
+            placeholder="Email"
+            className="w-full mt-4 border border-gray-200 rounded-md px-4 py-2 h-12"
+          />
+          <input
+            type="text"
+            placeholder="Password"
+            className="w-full mt-2 border border-gray-200 rounded-md px-4 py-2 h-12"
+          />
+        </div>
+        <div className="text-center text-[#637381] text-xs mt-4">Bằng cách đăng ký, tôi đồng ý với</div>
+        <div className="flex justify-center items-center w-full mt-1 gap-1">
+          <div className="underline text-xs">Quên mật khẩu?</div>
+          <div className="text-center text-[#637381] text-xs">và</div>
+          <div className="underline text-xs">Điều khoản sử dụng</div>
+        </div>
+        <button className="font-semibold text-white bg-[#2F57EF] rounded-xl mt-4 w-full h-12">
+          Tạo tài khoản
+        </button>
+        <div className="text-center text-[#637381] text-sm my-5">Hoặc</div>
+        <button className="font-semibold text-white bg-[#919EAB8f] rounded-xl w-full h-12 flex justify-center items-center gap-2">
+          <Image
+            src={logoGoogle}
+            alt="banner"
+            className="h-6 w-6 object-cover"
+          />
+          Đăng nhập với Google
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default RegisterPage;

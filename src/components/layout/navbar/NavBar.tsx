9ff232989f87175a607151231fb9031ac8923ca9
Minh Le <minh.lv@tinasoft.vn>
2025-05-17 06:49:20 +0700

    add config css, navbar, footer

---
+++ src/components/layout/navbar/NavBar.tsx
@@ -0,0 +1,282 @@
+"use client";
+
+import React, { useCallback, useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { Routes } from "@/lib/routes/routes";
+import { useAuthContext } from "@/context/AuthProvider";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import {
+  ArrowDown2,
+  BagHappy,
+  CloseCircle,
+  Heart,
+  SearchNormal,
+} from "iconsax-react";
+import { usePathname, useRouter } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown";
+import { DropdownMenuItem } from "@/components/ui/menu";
+import { NotificationBadge } from "@/components/ui/notification-badge";
+import SearchPopup from "@/components/layout/navbar/SearchPopup";
+
+function Navbar() {
+  const pathname = usePathname();
+  const router = useRouter();
+  const [scrolled, setScrolled] = useState(false);
+  const { isAuthenticated } = useAuthContext();
+  const [timeLeft, setTimeLeft] = useState(3300);
+  const [isFlashSale, setIsFlashSale] = useState(pathname === Routes.home);
+  const [openSearch, setOpenSearch] = useState(false);
+
+  useEffect(() => {
+    // Hàm xử lý bắt phím
+    const handleKeyDown = (event: any) => {
+      // Kiểm tra xem phím được bấm có phải là Command + K không
+      // metaKey đại diện cho phím Command trên MacBook
+      if ((event.metaKey || event.ctrlKey) && event.key === "k") {
+        // Ngăn chặn hành vi mặc định của trình duyệt
+        event.preventDefault();
+
+        // Mở hoặc đóng command palette
+        setOpenSearch((prevState) => !prevState);
+      }
+    };
+
+    // Thêm event listener khi component được mount
+    document.addEventListener("keydown", handleKeyDown);
+
+    // Cleanup event listener khi component bị unmount
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  // useEffect(() => {
+  //   if (isAuthenticated) {
+  //     refetch().then((res) => {
+  //       if (res.data) {
+  //         setListCart(res.data);
+  //       }
+  //     });
+  //   }
+  // }, [isAuthenticated]);
+
+  useEffect(() => {
+    setIsFlashSale(pathname === Routes.home);
+  }, [pathname]);
+
+  useEffect(() => {
+    if (timeLeft <= 0) {
+      return;
+    }
+
+    const timer = setInterval(() => {
+      setTimeLeft((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [timeLeft]);
+
+  // 🔹 Lấy số giờ, phút, giây từ timeLeft
+  const hours = Math.floor(timeLeft / 3600);
+  const minutes = Math.floor((timeLeft % 3600) / 60);
+  const seconds = timeLeft % 60;
+
+  const navigateToFlashSale = () => {};
+
+  const navigateToCheckout = () => {};
+
+  const navigateToProfile = () => {};
+
+  const handleNavigateToHome = () => {
+    router.push(Routes.home);
+  };
+
+  const handleNavigateToProduct = (slug: string) => {
+    console.log("----slug", slug);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 30) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const renderHeightHeader = useCallback(() => {
+    return 0;
+  }, []);
+
+  return (
+    <div className="">
+      {isFlashSale && (
+        <motion.div className="bg-gray-900 relative">
+          <div
+            onClick={() => setIsFlashSale(false)}
+            className="cursor-pointer p-2 absolute right-2 lg:top-[50%] lg:translate-y-[-50%] top-2"
+          >
+            <CloseCircle size="20" color="#FFFFFF" />
+          </div>
+          <div className="md:max-w-3xl max-w-sm lg:max-w-5xl xl:max-w-7xl mx-auto items-start lg:flex-row flex-col flex lg:items-center justify-center px-6 py-2">
+            <div
+              onClick={navigateToFlashSale}
+              className="cursor-pointer flex-col lg:flex-row flex gap-1 items-center"
+            >
+              <span className="text-white lg:text-lg text-base font-semibold py-1 px-2.5 bg-tertiary-main rounded-[10px]">
+                Hot
+              </span>
+              <span className="lg:text-base text-white text-xs pl-4">
+                Nhanh tay nhận ưu đãi đến <span className="text-secondary-main">{' '}20%</span>
+              </span>
+            </div>
+            <div className="flex gap-2 items-center pl-6">
+              <span className="text-sm font-semibold text-white">
+                KẾT THÚC TRONG
+              </span>
+              <div className="h-8 w-10 flex items-center justify-center bg-zinc-800 rounded-lg p-1.5 text-xl text-tertiary-light font-semibold">
+                {String(hours).padStart(2, "0")}
+              </div>
+              <div className="h-8 w-10 flex items-center justify-center bg-zinc-800 rounded-lg p-1.5 text-xl text-tertiary-light font-semibold">
+                {String(minutes).padStart(2, "0")}
+              </div>
+              <div className="h-8 w-10 flex items-center justify-center bg-zinc-800 rounded-lg p-1.5 text-xl text-tertiary-light font-semibold">
+                {String(seconds).padStart(2, "0")}
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      )}
+      <motion.nav
+        initial={{ y: -50, opacity: 0 }}
+        animate={{
+          y: scrolled || !isFlashSale ? 0 : renderHeightHeader(),
+          opacity: 1,
+        }}
+        transition={{ duration: 0.2 }}
+        className={`fixed left-0 w-full z-50 transition-all bg-primary-main duration-100 ease-in-out ${
+          scrolled
+            ? "bg-white/85 backdrop-blur-sm top-0"
+            : "bg-transparent"
+        }`}
+      >
+        <div className="w-full py-2">
+          <div className="flex justify-between items-center mx-auto md:max-w-3xl max-w-sm lg:max-w-5xl xl:max-w-7xl">
+            {/* Nav Routes */}
+            <div className="flex items-center space-x-8">
+              <Image
+                src="/images/logo.png"
+                alt="Logo"
+                width={182}
+                height={48}
+                onClick={handleNavigateToHome}
+                className="cursor-pointer"
+              />
+
+              <DropdownMenu>
+                <DropdownMenuTrigger className="hidden lg:block focus-visible:outline-none">
+                  <div className="flex gap-2 items-center cursor-pointer">
+                    <div>Khám phá</div>
+                    <ArrowDown2 size={16} color="#27272A" />
+                  </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  align="start"
+                  sideOffset={20}
+                  alignOffset={-10}
+                  className="grid grid-cols-3 grid-rows-3 bg-white border-none"
+                >
+                  {[1, 2, 2, 3].map((_) => (
+                    <DropdownMenuItem key={_} className="block">
+                      <Button
+                        variant="ghost"
+                        className="block w-full text-left text-normal text-text-primary font-normal hover:text-text-primary"
+                        onClick={() => handleNavigateToProduct("Khám phá")}
+                      >
+                        Khám phá
+                      </Button>
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+
+            {/* Buttons */}
+            <div className="flex space-x-6 items-center">
+              {isAuthenticated ? (
+                <div className="flex items-center space-x-4">
+                  <div
+                    onClick={() => setOpenSearch(true)}
+                    className="cursor-pointer hidden lg:flex  bg-zinc-600/4 items-center h-10 px-2 rounded-md space-x-2"
+                  >
+                    <div>
+                      <SearchNormal size={16} color="#71717B" />
+                    </div>
+                    <span className="p-1 bg-white text-sm rounded shadow">
+                      ⌘K
+                    </span>
+                  </div>
+                  <Button size="lg" type="button" variant="ghost">
+                    <Heart size="24" color="#71717B" />
+                  </Button>
+                  <NotificationBadge label={"1"}>
+                    <Button
+                      size="lg"
+                      type="button"
+                      variant="ghost"
+                      onClick={navigateToCheckout}
+                    >
+                      <BagHappy size="24" color="#71717B" />
+                    </Button>
+                  </NotificationBadge>
+
+                  {/*<NotificationBadge label="0">*/}
+                  {/*  <Button size="lg" type="button" variant="ghost">*/}
+                  {/*    <Notification size="24" color="#71717B" />*/}
+                  {/*  </Button>*/}
+                  {/*</NotificationBadge>*/}
+                  <Image
+                    onClick={navigateToProfile}
+                    src={"/images/avatar-author.png"} // Đường dẫn đến avatar
+                    alt="Avatar"
+                    width={40}
+                    height={40}
+                    className="cursor-pointer rounded-full h-[40px] w-[40px]"
+                  />
+                </div>
+              ) : (
+                <>
+                  <div
+                    onClick={() => setOpenSearch(true)}
+                    className="hidden lg:flex cursor-pointer  bg-zinc-600/4 items-center h-10 px-2 rounded-md space-x-2"
+                  >
+                    <div>
+                      <SearchNormal size={16} color="#71717B" />
+                    </div>
+                    <span className="p-1 bg-white text-sm rounded shadow">
+                      ⌘K
+                    </span>
+                  </div>
+                  <Button variant="ghost" className="h-10">Đăng nhập</Button>
+                  <Button variant="default" className="bg-primary-main h-10  shadow-md hover:shadow-xl hover:shadow-primary-main/20 transition-shadow duration-300 text-white px-4 py-1.5 rounded-[10px]">Bắt đầu miễn phí</Button>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+        <SearchPopup open={openSearch} setOpen={setOpenSearch} />
+      </motion.nav>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/components/ui/notification-badge.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Badge, BadgeProps} from './badge';
+import {cn} from '@/lib/utils';
+
+export interface NotificationBadgeProps extends BadgeProps {
+  label?: string | number;
+  show?: boolean;
+}
+
+export const NotificationBadge = ({
+                                    label,
+                                    className,
+                                    show,
+                                    children,
+                                    ...props
+                                  }: NotificationBadgeProps) => {
+  const showBadge =
+    typeof label !== 'undefined' && (typeof show === 'undefined' || show);
+  return (
+    <div className='inline-flex relative'>
+      {children}
+      {showBadge && (
+        <Badge
+          className={cn(
+            'absolute text-sm top-0 right-0 rounded-sm text-white',
+            typeof label !== 'undefined' && ('' + label).length === 0
+              ? 'translate-x-1 -translate-y-1 px-1 py-1.5'
+              : 'translate-x-0 -translate-y-1 px-1.5 py-0.5',
+            className
+          )}
+          {...props}
+        >
+          {'' + label}
+        </Badge>
+      )}
+    </div>
+  );
+};

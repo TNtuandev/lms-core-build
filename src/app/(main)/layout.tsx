337ce0caecdd0d3275401f22d1cb69f3df757ce4
Minh Le <minh.lv@tinasoft.vn>
2025-05-22 07:23:42 +0700

    merge code update home

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/layout/Footer";
 function MainLayout({ children }: PropsWithChildren) {
   return <>
     <Navbar />
-      <main className="flex-1 md:max-w-3xl max-w-sm lg:max-w-5xl xl:max-w-7xl mx-auto w-full">{children}</main>
+      <main className="flex-1">{children}</main>
     <Footer />
   </>;
 }

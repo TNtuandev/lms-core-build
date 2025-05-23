d8fe2d237584857782dccb815380929fc048ccbc
minhlv_vtp <minhlv24@viettelpost.com.vn>
2025-05-23 15:16:51 +0700

    update instructor

---
+++ src/contants/instructor/index.tsx
@@ -0,0 +1,47 @@
+export const TEACHER_LIST = [
+  {
+    images: '/images/instructor/img.png',
+    containerClass: 'col-span-3 row-span-3'
+  },
+  {
+    images: '/images/instructor/img_1.png',
+    containerClass: 'col-span-3 row-span-3'
+  },
+  {
+    images: '/images/instructor/img_2.png',
+    containerClass: 'col-span-3 row-span-3'
+  },
+  {
+    images: '/images/instructor/img_3.png',
+    containerClass: 'col-span-3 row-span-3'
+  },
+  {
+    images: '/images/instructor/img_4.png',
+    containerClass: 'col-span-2 row-span-2'
+  },
+
+  {
+    images: '/images/instructor/img_5.png',
+    containerClass: 'col-span-2 row-span-2'
+  },
+
+  {
+    images: '/images/instructor/img_6.png',
+    containerClass: 'col-span-2 row-span-2'
+  },
+
+  {
+    images: '/images/instructor/img_7.png',
+    containerClass: 'col-span-2 row-span-2'
+  },
+
+  {
+    images: '/images/instructor/img_8.png',
+    containerClass: 'col-span-2 row-span-2'
+  },
+
+  {
+    images: '/images/instructor/img_9.png',
+    containerClass: 'col-span-2 row-span-2'
+  },
+]

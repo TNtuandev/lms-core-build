f588d3ab3da88b07a5667fb6e8799e172b38645d
Minh chim to <tuan.na@tinasoft.com.vn>
2025-06-29 23:15:41 +0700

    feat: update course

--- src/api/endpoints/course.api.ts
+++ src/api/endpoints/course.api.ts
@@ -1,22 +1,29 @@
-import { CourseFilters, CoursesResponse, CourseDetail, RelatedCoursesResponse, FAQsResponse, ICreateCourseRequest, RelatedCourse, FAQ } from "@/api/types/course.type";
+import {
+  CourseFilters,
+  CoursesResponse,
+  CourseDetail,
+  ICreateCourseRequest,
+  RelatedCourse,
+  FAQ,
+} from "@/api/types/course.type";
 import api from "@/api/api";
 
 export const courseAPI = {
   getCourses: async (filters?: CourseFilters): Promise<CoursesResponse> => {
     const params = new URLSearchParams();
-    
+
     if (filters) {
       Object.entries(filters).forEach(([key, value]) => {
-        if (value !== undefined && value !== null && value !== '') {
+        if (value !== undefined && value !== null && value !== "") {
           if (Array.isArray(value)) {
-            value.forEach(item => params.append(key, item.toString()));
+            value.forEach((item) => params.append(key, item.toString()));
           } else {
             params.append(key, value.toString());
           }
         }
       });
     }
-    
+
     const { data } = await api.get(`/courses?${params.toString()}`);
     return data;
   },

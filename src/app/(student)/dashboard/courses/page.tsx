06c79be245ab6d10b1cfe4874c6d823325ced592
Minh Le <minhugvlog@gmail.com>
2025-08-18 07:17:19 +0700

    lint

--- src/app/(student)/dashboard/courses/page.tsx
+++ src/app/(student)/dashboard/courses/page.tsx
@@ -127,6 +127,7 @@ function EnrolledCoursesPage() {
       {/*  </div>*/}
       {/*)}*/}
 
+
       {/* Course Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {data?.map((item) => (
